0b1ca1c0253fe49ddecaf1c883d19fc7bd22a7ae
codycreech <codycreech@me.com>
2022-03-24 08:24:06 -0400

    updated docs

--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const socket = require('socket.io');
 const cors = require('cors');
 const mysql = require('mysql2');
 
+//Mysql connection info
 let con = mysql.createConnection({
   host: 'localhost',
   user: 'nodejs',
   password: 'password',
   database: 'sphere_db'
 });
+
+//Creates the server
 let app = express();
 let server = app.listen(3000, listen);
+
+//List of SQL queries
 let createTable = 'create table if not exists nodes(id int auto_increment primary key, a int not null, b int not null, x varchar(150) not null, y varchar(150) not null, z varchar(150) not null, color varchar(150) not null)';
 let stmt = 'INSERT INTO nodes(a,b,x,y,z,color) VALUES(?,?,?,?,?,?)';
 let upd = 'UPDATE nodes SET color = ? WHERE a = ? AND b = ?';
 let qry = 'SELECT a,b,x,y,z,color FROM nodes';
 
+//Creates initial table when a user connects to the site if it doesn't exist
 con.connect(function(err) {
   if(err) throw err;
   console.log('Connected to database!');
@@ -23,12 +29,13 @@ con.connect(function(err) {
     if(err) {
       console.log(err.message);
     }
-    // console.log("'nodes' table created.");
   });
 });
 
 app.use(express.static('public'));
 
+
+//Listens for new clients
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
@@ -44,6 +51,7 @@ var io = socket(server, {
   },
 });
 
+//Shows if client has disconnected
 io.on('disconnect', function() {
   console.log('Client has disconnected');
 });
@@ -58,9 +66,10 @@ io.on('connect', (socket) => {
     console.log('Sent table to client.');
   });
 
-  socket.on('data', data => {
-    console.log(data);
-  });
+  //Me testing sending/receiving data through a socket
+//  socket.on('data', data => {
+//    console.log(data);
+//  });
   
   //Update any changes to the table
   socket.on('update', table => {
@@ -75,7 +84,7 @@ io.on('connect', (socket) => {
       console.log('Updated nodes.');
   });
   
-  //Create initial table if it doesn't exist
+  //Loads initial table data into the db if it doesn't exist
   socket.on('save', table => {
       for(let i = 0; i < table.length; i++) {
         con.query(stmt, table[i], (err, results, fields) => {
@@ -87,7 +96,7 @@ io.on('connect', (socket) => {
       console.log('Loaded nodes.');
   });
   
-  //Get the table from the db and send to the client
+  //Get the table data from the db and send to the client
   socket.on('load', function() {
     con.query(qry, function(err, results, fields) {
       if(err) {
@@ -97,5 +106,7 @@ io.on('connect', (socket) => {
       console.log('Sent table to client.');
     });
   });
+  
+  //Shows the id for each client connected
   console.log('new connection: ' + socket.id);
 });
